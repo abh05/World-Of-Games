e02a434c452f57d952bda3c9a0dfa495527f4ea3
Avi Devops <aviben05@gmail.com>
2022-02-01 21:35:36 +0200

    changed add score method

--- Score.py
+++ Score.py
@@ -1,14 +1,28 @@
 from pathlib import Path
+path_file = Path("C:/Users/avi/PycharmProjects/WorldOfGames/Scores.txt")
+
+def get_score_from_file():
+    SCORES_FILE = open(path_file, "r+")
+    prevuisScore = SCORES_FILE.readlines()[0]
+    score_as_int = int(prevuisScore)
+    SCORES_FILE.close()
+    return score_as_int
+
+def write_score_to_file(score):
+    SCORES_FILE = open(path_file, "w")
+    SCORES_FILE.write(str(score))
+    SCORES_FILE.close()
+
+
 
 def add_score(difficulty):
     global Total
     POINTS_OF_WINNING = (difficulty * 3) + 5
-    path_file = Path("C:/Users/avi/PycharmProjects/WorldOfGames/Scores.txt")
     if path_file.is_file():
         SCORES_FILE = open(path_file, "r+")
-        prevuisscore = SCORES_FILE.readlines()
-        print(f"Old Score {int(prevuisscore[0])}")
-        for line in prevuisscore:
+        prevuisScore = SCORES_FILE.readlines()
+        print(f"Old Score {int(prevuisScore[0])}")
+        for line in prevuisScore:
             splitted_line = line.split(' ')
             for values in splitted_line:
                 value_as_int = int(values)
@@ -25,6 +39,24 @@ def add_score(difficulty):
         print(POINTS_OF_WINNING)
         SCORES_FILE.close()
 
+
+def add_score2(difficulty):
+    POINTS_OF_WINNING = (difficulty * 3) + 5
+    if not (path_file.is_file()):
+        write_score_to_file(POINTS_OF_WINNING)
+        print(f"Your Score is: {POINTS_OF_WINNING} Points")
+    else:
+        old_score = get_score_from_file()
+#       Finished getting old Value
+        print(f"Old Score {old_score}")
+        new_score = old_score + POINTS_OF_WINNING
+        print(f"New Score {new_score}")
+#       updating The New Score To File
+        write_score_to_file(new_score)
+
+
+
+        #homeWork Change the method so score is 0 once the file does not exsist - no Code Duplication
 
 
 
