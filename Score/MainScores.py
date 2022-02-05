aa049bb2a504ad2436c299812cb482a9f2e3339e
Avi Devops <aviben05@gmail.com>
2022-02-05 16:14:15 +0200

    moved to Score folder

--- Score/MainScores.py
+++ Score/MainScores.py
@@ -1,14 +1,14 @@
-import Utils
+from Utils import BAD_RETURN_CODE
 from flask import Flask, render_template
 from jinja2 import TemplateNotFound
 
-app = Flask(__name__, template_folder='C:/Users/avi/PycharmProjects/WorldOfGames/')
+app = Flask(__name__, template_folder='C:/Users/avi/PycharmProjects/WorldOfGames/Score/')
 
 @app.route('/score')
 def score_server():
-    error = Utils.BAD_RETURN_CODE
+    error = BAD_RETURN_CODE
     try:
-        SCORES_FILE  = open("C:/Users/avi/PycharmProjects/WorldOfGames/Scores.txt", "r")
+        SCORES_FILE  = open('C:/Users/avi/PycharmProjects/WorldOfGames/Score/Score.txt', "r")
         score = SCORES_FILE.readlines()
         score = int(score[0])
         return render_template("ScoreH.html", score=score)
