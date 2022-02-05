83a5fd184b3c5f9800b6209615a77f35d9dbc122
Avi Devops <aviben05@gmail.com>
2022-02-05 16:13:24 +0200

    Changed location to Score Folder and clean code without any comments

---
+++ Score/Score.py
@@ -0,0 +1,33 @@
+from Utils import SCORES_FILE_NAME
+
+def get_score_from_file():
+    SCORES_FILE = open(SCORES_FILE_NAME, "r+")
+    prevuisScore = SCORES_FILE.readlines()[0]
+    score_as_int = int(prevuisScore)
+    SCORES_FILE.close()
+    return score_as_int
+
+def write_score_to_file(score):
+    SCORES_FILE = open(SCORES_FILE_NAME, "w")
+    SCORES_FILE.write(str(score))
+    SCORES_FILE.close()
+
+
+def add_score(difficulty):
+    POINTS_OF_WINNING = (difficulty * 3) + 5
+    old_score = 0
+    if SCORES_FILE_NAME.is_file():
+    #getting old Value
+        old_score = get_score_from_file()
+    print(f"Old Score {old_score}")
+    new_score = old_score + POINTS_OF_WINNING
+    print(f"New Score {new_score}")
+    # updating The New Score To File
+    write_score_to_file(new_score)
+
+
+
+
+
+
+
