c363c7054320125531c4cdba73953f7c8c85d26e
Avi Devops <aviben05@gmail.com>
2022-02-20 18:00:16 +0200

    Removed local paths

--- Score/MainScores.py
+++ Score/MainScores.py
@@ -1,14 +1,15 @@
-from Utils import BAD_RETURN_CODE
 from flask import Flask, render_template
 from jinja2 import TemplateNotFound
+from Utils import BAD_RETURN_CODE
 
-app = Flask(__name__, template_folder='C:/Users/avi/PycharmProjects/WorldOfGames/Score/')
+app = Flask(__name__)
+
 
 @app.route('/score')
 def score_server():
     error = BAD_RETURN_CODE
     try:
-        SCORES_FILE  = open('C:/Users/avi/PycharmProjects/WorldOfGames/Score/Score.txt', "r")
+        SCORES_FILE = open('Score.txt', "r")
         score = SCORES_FILE.readlines()
         score = int(score[0])
         return render_template("ScoreH.html", score=score)
@@ -17,4 +18,3 @@ def score_server():
 
 
 app.run(port=40000, debug=True)
-
